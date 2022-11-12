3ef73868c2ece9915c01f2306cc021e26225338a
SHC-MyAccount <sunshcchao@163.com>
2022-11-12 09:07:31 +0800

    "d"

---
+++ basic/src/main/java/com/shc/basic/BasicMain.java
@@ -0,0 +1,12 @@
+package com.shc.basic;
+
+/**
+ * @author shc
+ * @date 2022-11-12
+ */
+public class BasicMain {
+
+    public static void main(String[] args) {
+        System.out.println("basic");
+    }
+}
