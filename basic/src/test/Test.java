8b917bae76fdc4a19202c11e834bd23d4fb4fac3
SHC-MyAccount <sunshcchao@163.com>
2022-11-12 08:36:50 +0800

    create

---
+++ basic/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author shc
+ * @date 2022-11-06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("aaaaaa");
+
+    }
+}
---
+++ tools/src/main/java/com/shc/tools/ToolsMain.java
@@ -0,0 +1,12 @@
+package com.shc.tools;
+
+/**
+ * @author shc
+ * @date 2022-11-06
+ */
+public class ToolsMain {
+
+    public static void main(String[] args) {
+        System.out.println("afasfsf");
+    }
+}
